4607d4d85ef63117575f97706d7e7e6b8c1ef71b
xmp-er <107166230+xmp-er@users.noreply.github.com>
2023-12-27 01:42:31 +0530

    Modifications for createMovieHandler so proper Runtime format is accepted

--- internal/data/runtime.go
+++ internal/data/runtime.go
@@ -1,9 +1,13 @@
 package data
 
 import (
+	"errors"
 	"fmt"
 	"strconv"
+	"strings"
 )
+
+var invalidRuntimeFormat = errors.New("Invalid Runtime Format")
 
 type Runtime int32
 
@@ -13,4 +17,28 @@ func (r Runtime) MarshalJSON() ([]byte, error) {
 	quoted_data := strconv.Quote(data)
 
 	return []byte(quoted_data), nil
+}
+
+func (r *Runtime) UnmarshalJSON(jsonValue []byte) error {
+	unquotedJsonValue, err := strconv.Unquote(string(jsonValue))
+
+	if err != nil {
+		return invalidRuntimeFormat
+	}
+
+	parts := strings.Split(unquotedJsonValue, " ")
+
+	if len(parts) != 2 || parts[1] != "mins" {
+		return invalidRuntimeFormat
+	}
+
+	final, err := strconv.ParseInt(parts[0], 10, 32)
+
+	if err != nil {
+		return invalidRuntimeFormat
+	}
+
+	*r = Runtime(final) //converting to Runtime datatype
+
+	return nil
 }
