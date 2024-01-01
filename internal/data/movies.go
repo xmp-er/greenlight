ef1eb50b3abf689437da0cfd9fa00bfc3e78fbf0
xmp-er <107166230+xmp-er@users.noreply.github.com>
2024-01-01 19:56:29 +0530

    Created indexes using Gin for faster lookup and search for when we are querying all lists

--- internal/data/movies.go
+++ internal/data/movies.go
@@ -136,7 +136,7 @@ func (m MovieModel) Get(id int64) (*Movie, error) {
 }
 
 func (m MovieModel) GetAll(title string, genres []string, filter Filters) ([]*Movie, error) {
-	query := `SELECT id, created_at, title, year, runtime, genres, version FROM movies WHERE (LOWER(title) = LOWER($1) OR $1='') AND (genres @> $2 OR $2='{}') ORDER BY id`
+	query := `SELECT id, created_at, title, year, runtime, genres, version FROM movies WHERE (to_tsvector('simple', title) @@ plainto_tsquery('simple', $1) OR $1 = '') AND (genres @> $2 OR $2='{}') ORDER BY id`
 
 	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
 
