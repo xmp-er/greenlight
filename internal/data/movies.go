aee70714191078f46e23a2f31f82973356b41c0b
xmp-er <107166230+xmp-er@users.noreply.github.com>
2024-01-01 21:36:28 +0530

    Added pagination support for returning data

--- internal/data/movies.go
+++ internal/data/movies.go
@@ -142,11 +142,11 @@ func (m MovieModel) GetAll(title string, genres []string, filters Filters) ([]*M
 	FROM movies
 	WHERE (to_tsvector('simple', title) @@ plainto_tsquery('simple', $1) OR $1 = '') 
 	AND (genres @> $2 OR $2 = '{}')     
-	ORDER BY %s %s, id ASC`, filters.sortColumn(), filters.sortDirection())
+	ORDER BY %s %s, id ASC LIMIT $3 OFFSET $4`, filters.sortColumn(), filters.sortDirection())
 
 	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
 
-	rows, err := m.DB.QueryContext(ctx, query, title, pq.Array(genres))
+	rows, err := m.DB.QueryContext(ctx, query, title, pq.Array(genres), filters.limit(), filters.offset())
 
 	defer cancel()
 
