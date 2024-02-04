3c45a60812920d449f32d436406a55a9bcb5994a
xmp-er <107166230+xmp-er@users.noreply.github.com>
2024-02-04 13:43:05 +0530

    Method added for getting activation token

--- cmd/api/users.go
+++ cmd/api/users.go
@@ -3,12 +3,12 @@ package main
 import (
 	"errors"
 	_ "errors"
+	"log"
 	"net/http"
+	"time"
 
 	"greenlight.architsproject/internal/data"
-	_ "greenlight.architsproject/internal/data"
 	"greenlight.architsproject/internal/validator"
-	_ "greenlight.architsproject/internal/validator"
 )
 
 func (app *application) registerUserHandler(w http.ResponseWriter, r *http.Request) {
@@ -60,20 +60,30 @@ func (app *application) registerUserHandler(w http.ResponseWriter, r *http.Reque
 	}
 
 	err = app.convertDataToJson(w, http.StatusCreated, envelope{"user": user}, nil)
-
 	if err != nil {
 		app.serverErrorResponse(w, r)
 	}
 
+	token, err := app.models.Tokens.New(user.ID, 3*24*time.Hour, data.ScopeActivation)
+
 	app.background(func() {
-		err = app.mailer.Send(user.Email, "user_welcome.tmpl", user)
+		data := map[string]any{
+			"activationToken": token.Plaintext,
+			"userID":          user.ID,
+		}
+		err = app.mailer.Send(user.Email, "user_welcome.tmpl", data)
 		if err != nil {
 			app.logger.PrintError(err, nil)
 		}
 	})
-
-	err = app.convertDataToJson(w, http.StatusCreated, envelope{"user": user}, nil)
 	if err != nil {
+		app.serverErrorResponse(w, r)
+		return
+	}
+
+	err = app.convertDataToJson(w, http.StatusAccepted, envelope{"user": user}, nil)
+	if err != nil {
+		log.Println(err)
 		app.serverErrorResponse(w, r)
 	}
 
